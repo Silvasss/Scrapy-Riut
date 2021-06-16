5983f60c396dcfb6f2ef5a0f6df21366660ebd27
Silvasss <felipe_silva55@hotmail.com>
2021-06-16 19:27:07 -0300

    New version 2.0

--- riut/riut/spiders/example.py
+++ riut/riut/spiders/example.py
@@ -1,13 +1,24 @@
 import scrapy
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 
-# https://repositorio.utfpr.edu.br/jspui/handle/1/20556
-# response.css("tr ::text").getall()
 
 class ExampleSpider(scrapy.Spider):
     name = 'example'
 
-    start_urls = ["https://repositorio.utfpr.edu.br/jspui/handle/1/20556"]
+    start_urls = []
 
+    # Criando os links dos repositorios
+    for i in range(0, 25300):
+        start_urls.append("https://repositorio.utfpr.edu.br/jspui/handle/1/{}".format(i))
+
+    # https://docs.scrapy.org/en/latest/topics/request-response.html#topics-request-response-ref-request-callback-arguments
+    def start_requests(self):
+        for i in self.start_urls:
+            yield scrapy.Request(i, callback = self.parse, errback = self.errback_httpbin, dont_filter = True)
+
+    # Faz acontece, caso nao ocorra nenhum erro
     def parse(self, response):
         camposInteresse = ["Título:", "Autor(es):", "Orientador(es):", "Data do documento:", "Resumo:", "URI:"]
 
@@ -25,6 +36,7 @@ class ExampleSpider(scrapy.Spider):
             except NameError:
                 pass
 
+        # Erro em alguns por nao localizar o "Orientador(es):\xa0"
         yield {
             "Título" : dictAux["Título:\xa0"],
             "Autor(es)" : dictAux["Autor(es):\xa0"],
@@ -34,3 +46,22 @@ class ExampleSpider(scrapy.Spider):
             "Resumo" : dictAux["Resumo:\xa0"],
             "Repositorio" : dictAux["URI:\xa0"]
         }
+
+    def errback_httpbin(self, failure):
+        # log all failure
+        self.logger.error(repr(failure))
+
+        if failure.check(HttpError):
+            # these exception come from HttpError spider middleware
+            # get the non-200 response
+            response = failure.value.response
+            self.logger.error('HttpError on %s', response.url)
+
+        elif failure.check(DNSLookupError):
+            # this is original request
+            request = failure.request
+            self.logger.error('DnsLookupError on %s', request.url)
+
+        elif failure.check(TimeoutError, TCPTimedOutError):
+            request = failure.request
+            self.logger.error('TimeoutError on %s', request.url)
